19f2e37d0524bfe5da1b34c1de87682f0e274fca
Szymon "l7ssha" Uglis <szymon.uglis@hotmail.com>
2016-04-13 16:12:11 +0200

    Code Optimalization & licenses

--- LsevenS/main.cpp
+++ LsevenS/main.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
+/*
+------------------------- l7s BY L7SSHA | ALL RIGTS RESERVED -------------------------
+l7s version 1.0
+Copyright (c) 2016 Szymon "l7ssha" Uglis
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to no one to use this software for any purpose,
+including commercial application
+
+------------------------- l7s BY L7SSHA | ALL RIGTS RESERVED -------------------------
+*/
 #include "MainGame.h"
 
 int main(int argc, char** argv) {
     MainGame mainGame;
-    mainGame.run();
 
+    mainGame.run();
     return 0;
 }
